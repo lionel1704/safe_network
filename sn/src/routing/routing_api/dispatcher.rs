4abc564bf947cd7d2af59879d1e5194f5ded2c92
Josh Wilson <joshuef@gmail.com>
2021-11-26 14:05:33 +0100

    fix: resend join on wrong sig

--- sn/src/routing/routing_api/dispatcher.rs
+++ sn/src/routing/routing_api/dispatcher.rs
@@ -276,8 +276,10 @@ impl Dispatcher {
         command: Command,
         cmd_id: Option<String>,
     ) -> Result<()> {
+        debug!("staeting enqueue command {:?}", cmd_id);
         let _ = tokio::spawn(async {
             let cmd_id = cmd_id.unwrap_or_else(|| rand::random::<u32>().to_string());
+            debug!("spawnes enqueue command {:?}", cmd_id);
             self.acquire_permit_or_wait(command.priority()?, cmd_id.clone())
                 .await;
             self.handle_command_and_any_offshoots(command, cmd_id).await
