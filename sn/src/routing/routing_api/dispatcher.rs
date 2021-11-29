aa3a9d62310b5488ca6e0d0d3e80b5aee6c20d1f
Josh Wilson <joshuef@gmail.com>
2021-11-29 07:57:40 +0100

    chore: tidy up some log messages

--- sn/src/routing/routing_api/dispatcher.rs
+++ sn/src/routing/routing_api/dispatcher.rs
@@ -96,7 +96,11 @@ impl Dispatcher {
         // there's probably a neater way to do this
         while semaphore.available_permits() != count {
             time::sleep(Duration::from_millis(1000)).await;
-            debug!("available, permits {:?}", semaphore.available_permits());
+            trace!(
+                "looping while we wait for available permits to be {:?}: {:?}",
+                count,
+                semaphore.available_permits()
+            );
         }
     }
 
